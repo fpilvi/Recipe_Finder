5b959896cae8f00338c0f9083018a36272460322
fpilvi <florentine.moehrle@gmail.com>
2025-04-08 15:27:53 +0200

    valid hashed passwor d

--- app/auth.py
+++ app/auth.py
@@ -42,7 +42,7 @@ class UserResponse(BaseModel):
 
 
 def hash_password(password: str):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
 
 def verify_password(plain_password: str, hashed_password) -> bool:
